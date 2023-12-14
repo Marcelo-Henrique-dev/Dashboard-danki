a84bf5a220afdf06ccdbeee277866ddca784c8fa
Marcelo-Henrique_dev <dvmarcelohenrique@gmail.com>
2023-12-14 14:13:36 -0300

    adicionado caixa de notificações e responsivo

--- js/script.js
+++ js/script.js
@@ -1,7 +1,18 @@
 const btnSearch = document.getElementById('btn-search')
 const inpt = document.querySelector('.pesquisa-campo input[type=text]')
-
+const btnMenu = document.querySelector('.nome-user')
+const menu = document.querySelector('.sidebar')
+const notifBtn = document.querySelector('.icon-alert')
+const boxNotifications = document.querySelector('.notification-box')
 btnSearch.addEventListener('click', ()=>{
     inpt.classList.toggle('active')
     btnSearch.classList.toggle('active')
+})
+
+btnMenu.addEventListener('click', ()=>{
+    menu.classList.toggle('sidebar-on')
+})
+
+notifBtn.addEventListener('click', ()=>{
+    boxNotifications.classList.toggle('notification-on')
 })
